31ca80b558f23f793373fae56c95d308450cf4dc
codri <codri@VIRTUAL>
2024-02-15 22:47:17 +0200

    Add project files.

---
+++ Lab1Ex8WON7/Program.cs
@@ -0,0 +1,6 @@
+﻿/*Ex8
+Se citesc trei numere de la tastatura, x,y,z. Scrieti un program care va afisa cele
+trei valori in ordine descrescatoare.
+• Exemplu: citim 3 9 0 Afisam : 9 3 0*/
+const uint numarVariabile=3;
+ShowNumbers(SortDescending(ReadNumbers(numarVariabile)));
---
+++ Lab1Ex8WON7/ProgramHelpers.cs
@@ -0,0 +1,34 @@
+﻿partial class Program
+{
+    static int[] ReadNumbers(uint numarVariabile)
+    {
+        Console.WriteLine($"Introduceti {numarVariabile} numere pentru a le sorta descrescator");
+        var numere = new int[numarVariabile];
+        var numereRaw = Console.ReadLine()?.Split(" ");
+        if (numereRaw?.Length == numarVariabile) { 
+            for (int i = 0; i < numereRaw.Length; i++)
+                if (!int.TryParse(numereRaw[i], out numere[i]))
+                {
+                    Console.WriteLine("Nu ati introdus datele in format valid, mai incercati");
+                    ReadNumbers(numarVariabile);
+                } 
+        }
+        else
+        {
+            Console.WriteLine("Nu ati introdus datele in format valid, mai incercati");
+            ReadNumbers(numarVariabile);
+        }
+        return numere;
+    }
+    static int[] SortDescending(int[] numere)
+    {
+        Array.Sort(numere);
+        Array.Reverse(numere);
+        return numere;
+    }
+    static void ShowNumbers(int[] numere)
+    {
+        foreach(var numar in numere)
+            Console.Write(numar + " ");
+    }
+}
