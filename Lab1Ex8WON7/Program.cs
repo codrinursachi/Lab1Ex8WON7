273d11a4766d134c6b704718bf2cfe20e2887b01
codrinursachi <codrinursachi@gmail.com>
2024-02-17 09:38:45 +0200

    Formatting

--- Lab1Ex8WON7/Program.cs
+++ Lab1Ex8WON7/Program.cs
@@ -1,6 +1,9 @@
-﻿/*Ex8
-Se citesc trei numere de la tastatura, x,y,z. Scrieti un program care va afisa cele
-trei valori in ordine descrescatoare.
+﻿/*Ex8
+
+Se citesc trei numere de la tastatura, x,y,z. Scrieti un program care va afisa cele
+
+trei valori in ordine descrescatoare.
+
 • Exemplu: citim 3 9 0 Afisam : 9 3 0*/
-const uint numarVariabile=3;
-ShowNumbers(SortDescending(ReadNumbers(numarVariabile)));
+const uint NUMAR_VARIABILE=3;
+ShowNumbers(SortDescending(ReadNumbers(NUMAR_VARIABILE)));
